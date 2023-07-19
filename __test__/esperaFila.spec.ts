c17b4e2ccdc8cdee99cf0a16ac2cf401049c7136
geangontijo <geangontijo07@gmail.com>
2023-07-19 13:18:43 -0300

    finalizando fila do opensearch #2987-problema-pagamento-MS-com-pixs-pendente

--- __test__/esperaFila.spec.ts
+++ __test__/esperaFila.spec.ts
@@ -1,49 +1,9 @@
-import {esperaFila, RespostaFila} from '../index'
-/*
+import { esperaFila } from '../index'
+
 let timeout = 500000;
-it("deve tentar duas vezes e na terceira resolver com sucesso.", async () => {
-  let contagemTentativas = 1;
-  const respostaFila = await esperaFila('api_pagamento/link_pagamento/fila/');
-
-  expect(respostaFila.resposta).toStrictEqual({
-    message: "Deu tudo certo."
-  })
-  expect(respostaFila.situacao).toBe('OK')
-}, timeout);
-
-it("deve tentar duas vezes e na terceira resolver com erro.", async () => {
-  let contagemTentativas = 1;
-  let respostaFila = await esperaFila(
-    () => new Promise<RespostaFila<{ message: string }>>((resolve) => {
-      if (contagemTentativas === 3) {
-        return resolve({
-          situacao: 'ER',
-          resposta: {
-            message: 'Os dados enviados estão inválidos.'
-          }
-        })
-      }
-
-      resolve({
-        situacao: 'PE'
-      })
-      contagemTentativas++
-    })
-  );
-
-  expect(respostaFila.resposta).toStrictEqual({
-    message: 'Os dados enviados estão inválidos.'
-  })
-  expect(respostaFila.situacao).toBe('ER')
-}, timeout)
-
 it('deve tentar uma vez e dar erro.', async () => {
   try {
-    await esperaFila(() => new Promise((_, reject) => {
-      reject({
-        message: "Erro ao buscar o item da fila."
-      })
-    }));
+    await esperaFila('3');
   } catch (err) {
     expect(err).toStrictEqual({
       message: "Erro ao buscar o item da fila."
